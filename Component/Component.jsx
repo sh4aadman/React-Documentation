ad0d638a2d8532230a021a190169adb563d445e9
sh4aadman <sh4adman@gmail.com>
2026-01-25 16:01:05 +0600

    25-01-26

---
+++ Component/Component.jsx
@@ -0,0 +1,22 @@
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
+export default function Gallery() {
+    return (
+        <section>
+            <h1>Amazing Scientists</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
+}
+
+// React turns the components into a node in DOM and updates the Virtual Dom first through Diffing Algorithm.
+
+// React.createElement(type, props, children)
+
+// Diffing Algorithm compares the new tree (usually after a user interaction or data change) with older one starting from root element and moving down recursively. And then patches to actual DOM.
+
+// Reducing complexity to O(n ^ 3) [complexity of a complete tree comparison] to O(n)
