9684d754168f7f83d5e2442a8da24261d5e61ad2
sh4aadman <sh4adman@gmail.com>
2026-01-27 00:14:40 +0600

    27-01-26

--- 4. CurlyBraces/CurlyBraces.jsx
+++ 4. CurlyBraces/CurlyBraces.jsx
@@ -42,3 +42,17 @@ export function TodoList() {
 }
 
 // Double Curly Braces are used to use JavaScript Object inside JSX. Usage - inline style - as style object
+
+const element = <h1 id={myId}>{content}</h1>;
+// The braces are gone; the code inside is now just function arguments.
+// const element = React.createElement('h1', { id: myId }, content);
+
+// Braces after equal sign - tell the compiler to put that value directly into the props object (second argument).
+// Braces between tags - tell the comppiler to pass that value as a child element (third argument).
+
+// Automatic Sanitization - to prevent Cross-Site Scripting(XSS) - React automatically escapes any string values before rendering them to the DOM.
+
+// You cannot put an if statement or a for loop there because you cannot pass a statement as a function argument in JavaScript. Instead -
+// 1. Conditional Rendering / Logical AND - returns expression.
+// 2. Looping - map(), filter(), reduce(), find(), findIndex(), concat(), (... spread) - as it returns a new array.
+// 3. Logic outside JSX - outside of return block.
