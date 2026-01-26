8c70698e4039d1bdbd0e6a8fd11cf1c8b933be9b
sh4aadman <sh4adman@gmail.com>
2026-01-26 22:48:10 +0600

    26-01-26

---
+++ 4. CurlyBraces/CurlyBraces.jsx
@@ -0,0 +1,49 @@
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
+  return (
+    <h1>To Do List for {formatDate(today)}</h1>
+  );
+}
+
+// Curly Braces help to write JavaScript code inside JSX.
+
+// Curly Braces are used inside JSX tag content or right after "=" in attributes.
+
+const baseUrl = 'https://i.imgur.com/';
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 's',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
+        alt={person.name}
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
+
+// Double Curly Braces are used to use JavaScript Object inside JSX. Usage - inline style - as style object
